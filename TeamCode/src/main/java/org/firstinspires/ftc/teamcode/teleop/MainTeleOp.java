30141453b1a9c32b7c29a15f71927e3737f8b68b
zacherypan <zacherypanma@gmail.com>
2023-09-22 17:01:09 -0400

    dt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -1,5 +1,42 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-public class MainTeleOp {
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.geometry.Vector2d;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.teleop.subsystems.Bot;
+
+
+@TeleOp(name = "MainTeleOp", group = "Main")
+public class MainTeleOp extends LinearOpMode {
+
+    private Bot bot;
+
+    private double driveSpeed = 1;
+    private GamepadEx gp1, gp2;
+
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+        while(opModeIsActive() && !isStopRequested()) {
+            drive();
+        }
+    }
+
+    private void drive() {
+        driveSpeed = 1;
+        driveSpeed *= 1 - 0.5 * gp1.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+        driveSpeed = Math.max(0, driveSpeed);
+        Vector2d driveVector = new Vector2d(gp1.getLeftX(), -gp1.getLeftY()),
+                turnVector = new Vector2d(
+                        gp1.getRightX(), 0);
+        bot.drive(driveVector.getX() * driveSpeed,
+                driveVector.getY() * driveSpeed,
+                turnVector.getX() * driveSpeed / 1.7
+        );
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/subsystems/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/subsystems/Bot.java
@@ -1,5 +1,40 @@
 package org.firstinspires.ftc.teamcode.teleop.subsystems;
 
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 public class Bot {
+
+    public static Bot instance;
+    private final MotorEx fl, fr, bl, br;
+
+    private Bot(OpMode opMode) {
+        fl = new MotorEx(opMode.hardwareMap, "motorFL");
+        fr = new MotorEx(opMode.hardwareMap, "motorFR");
+        bl = new MotorEx(opMode.hardwareMap, "motorBL");
+        br = new MotorEx(opMode.hardwareMap, "motorBR");
+    }
+
+    public void drive(double strafeSpeed, double forwardBackSpeed, double turnSpeed) {
+        double[] speeds = {
+                forwardBackSpeed - strafeSpeed - turnSpeed,
+                forwardBackSpeed + strafeSpeed + turnSpeed,
+                forwardBackSpeed + strafeSpeed - turnSpeed,
+                forwardBackSpeed - strafeSpeed + turnSpeed
+        };
+        double maxSpeed = 0;
+        for (int i = 0; i < 4; i++) {
+            maxSpeed = Math.max(maxSpeed, speeds[i]);
+        }
+        if (maxSpeed > 1) {
+            for (int i = 0; i < 4; i++) {
+                speeds[i] /= maxSpeed;
+            }
+        }
+        fl.set(speeds[0]);
+        fr.set(speeds[1]);
+        bl.set(speeds[2]);
+        br.set(speeds[3]);
+    }
 
 }
