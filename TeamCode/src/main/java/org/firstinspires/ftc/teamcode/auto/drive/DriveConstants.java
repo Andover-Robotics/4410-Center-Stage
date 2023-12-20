35570367d75077763ee74292c928ff8e1dbfc741
raahilparikh <raahilparikh@gmail.com>
2023-12-19 19:18:45 -0500

    retuned Feedforward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/drive/DriveConstants.java
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.014;
-    public static double kA = 0.0045;
-    public static double kStatic = 0.01;
+    public static double kV = 0.013;
+    public static double kA = 0.0033;
+    public static double kStatic = 0.003;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -85,7 +85,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = ((MAX_RPM / 60) * GEAR_RATIO * WHEEL_RADIUS * 2 * Math.PI) * 0.85;//73.173
+    public static double MAX_VEL = 50;
     public static double MAX_ACCEL = ((MAX_RPM / 60) * GEAR_RATIO * WHEEL_RADIUS * 2 * Math.PI) * 0.55;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
